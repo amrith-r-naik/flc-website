cdf7d4915c0bdb6de308f0178fa14e3b63547714
Rakshith <rakshithx09@gmail.com>
2024-06-26 14:34:44 +0530

    feat: events page migrated from app to pages router

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
-const index = () => {
+import React from "react";
+import Card from "~/components/Card";
+import RadioButtons from "~/components/RadioButtons";
+function page() {
   return (
-    <div className='bg-black h-screen'>Event</div>
-  )
+    <>
+      <div className="flex justify-center">
+        <h1 className="text-gradient text-7xl font-bold">Events</h1>
+      </div>
+
+      <div className="flex justify-center py-16">
+        <RadioButtons />
+      </div>
+
+      <div className="mt-8 flex justify-center">
+        <Card />
+      </div>
+    </>
+  );
 }
 
-export default index
+export default page;
