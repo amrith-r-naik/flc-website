3ad6de050089a62b351e1ff0fb48c16ad0e8280e
Vishnu Bhat <vishnubhat013@gmail.com>
2024-06-27 12:10:14 +0530

    Feat: Event page Responsive

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -5,14 +5,19 @@ function page() {
   return (
     <>
       <div className="flex justify-center">
-        <h1 className="text-gradient text-7xl font-bold">Events</h1>
+        <h1 className="text-gradient mt-8 text-7xl font-bold">Events</h1>
       </div>
 
-      <div className="flex justify-center py-16">
+      <div className="flex justify-center py-8 md:py-16">
         <RadioButtons />
       </div>
 
-      <div className="mt-8 flex justify-center">
+      <div className=" mx-2 mt-8 flex flex-wrap justify-center gap-20 md:mx-8">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
       </div>
     </>
