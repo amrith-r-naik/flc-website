e4f3b7e52850651072aa0d2a327b3dad502fd99f
Nandan R Pai <nandanrp09@gmail.com>
2024-07-06 17:21:46 +0530

    update: user links router

--- src/server/api/routers/userLink.ts
+++ src/server/api/routers/userLink.ts
@@ -1,26 +1,14 @@
-import { z } from "zod";
-import { createTRPCRouter,protectedProcedure, publicProcedure } from "../trpc";
-import type { Prisma } from "@prisma/client";
+import {
+  createUserLinkZ,
+  deleteUserLinkZ,
+  getUserLinksZ,
+} from "~/zod/userLinkZ";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
 
-const createType= z.object({
-        url: z.string(),
-        linkName: z.string(),
-        userId: z.string(),
-      })
-
-const updateType = z.object({
-  id:z.string(),
-  newUrl: z.string().optional(),
-  newLinkName: z.string().optional(),
-  newUserId: z.string().optional(),
-});
-
-
-
-//switch to protectedProcedure after auth is done
 export const userLinkRouter = createTRPCRouter({
-  createUserLink: protectedProcedure /* change it back to protectedProcedure once auth is integrated */
-    .input(createType)
+  createUserLink: protectedProcedure
+    .input(createUserLinkZ)
     .mutation(async ({ ctx, input }) => {
       const duplicate = await ctx.db.userLink.findFirst({
         where: {
@@ -29,8 +17,12 @@ export const userLinkRouter = createTRPCRouter({
       });
 
       if (duplicate) {
-        throw new Error(`This link alreadty exist with id: ${duplicate.id}`);
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `This link alreadty exist with id: ${duplicate.id}`,
+        });
       }
+
       const newUserLink = await ctx.db.userLink.create({
         data: {
           url: input.url,
@@ -41,14 +33,10 @@ export const userLinkRouter = createTRPCRouter({
       return newUserLink;
     }),
 
-  getAllUserLinks: protectedProcedure.query(async ({ ctx }) => {
-    return await ctx.db.userLink.findMany();
-  }),
-
-  getOneUserLink: protectedProcedure
-    .input(z.object({ userId: z.string() }))
+  getUserLinks: protectedProcedure
+    .input(getUserLinksZ)
     .query(async ({ ctx, input }) => {
-      return await ctx.db.userLink.findFirst({
+      return await ctx.db.userLink.findMany({
         where: {
           userId: input.userId,
         },
@@ -58,35 +46,13 @@ export const userLinkRouter = createTRPCRouter({
       });
     }),
 
-  updateUserLink: protectedProcedure
-    .input(updateType)
-    .mutation(async ({ ctx, input }) => {
-      const userUrl = await ctx.db.userLink.findFirst({
-        where: { id: input.id },
-      });
-      if (!userUrl) {
-        throw new Error("No such UserUrl entity");
-      }
-
-      return await ctx.db.userLink.update({
-        where: {
-          id: input.id,
-        },
-        data: {
-          url: input.newUrl ?? userUrl.url,
-          linkName: input.newLinkName ?? userUrl.linkName,
-          userId: input.newUserId ?? userUrl.userId,
-        },
-      });
-    }),
-
   deleteUserLink: protectedProcedure
-    .input(z.object({ id: z.string(), url: z.string().optional() }))
+    .input(deleteUserLinkZ)
     .mutation(async ({ ctx, input }) => {
       return await ctx.db.userLink.delete({
         where: {
-          ...input,
-        } as Prisma.UserLinkWhereUniqueInput,
+          id: input.id,
+        },
       });
     }),
 });
