3dfaa4c8728e5230ac71dc4ff6544a24b7798488
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-07-19 10:34:38 +0530

    feat: show qr and edit option in profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronDown, ChevronUp, Pencil, X } from "lucide-react";
 import ImageCarousel from "~/components/imageCarousel";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-
+import * as Dialog from "@radix-ui/react-dialog";
 const Profile = () => {
   let seeMoreTimeline: gsap.core.Timeline, seeLessTimeline: gsap.core.Timeline;
+
+  const router = useRouter();
 
   gsap.registerPlugin(useGSAP);
 
@@ -91,67 +94,6 @@ const Profile = () => {
         },
         0,
       );
-
-    seeLessTimeline = gsap.timeline({ paused: true });
-    seeLessTimeline
-      .to(
-        ".BackgroundDesign",
-        {
-          autoAlpha: 1,
-        },
-        0,
-      )
-      .to(
-        [".SeeMoreOption"],
-        {
-          display: "flex",
-          opacity: 100,
-          position: "static",
-          duration: 0,
-        },
-        0,
-      )
-      .to(
-        [
-          ".SeeLessOption",
-          ".BioSection",
-          ".YearBranchSection",
-          ".ActivityPointSection",
-          ".AttendanceSection",
-          ".CertificatesCard",
-          ".CertificatesHeading",
-        ],
-        {
-          display: "none",
-        },
-        0,
-      )
-      .to(
-        ".CardsContainer",
-        {
-          bottom: 0,
-          top: "auto",
-          gap: 2,
-        },
-        0,
-      )
-      .to(
-        ".EventsHeading",
-        {
-          paddingLeft: "auto",
-          marginBottom: "-0.25rem",
-          marginTop: "auto",
-        },
-        0,
-      )
-      .to(
-        ".TopCard",
-        {
-          border: "2px solid var(--border)",
-          backgroundColor: "var(--card)",
-        },
-        0,
-      );
   });
 
   const images: string[] = [
@@ -186,6 +128,111 @@ const Profile = () => {
               className="rounded-full object-cover"
             />
           </div>
+
+          {/* QR and Edit Options */}
+          <div className="absolute flex w-full justify-between px-2 py-2">
+            <Dialog.Root>
+              <Dialog.Trigger className="rounded-sm border border-white border-opacity-10 bg-white bg-opacity-5 px-2 text-xs">
+                Show QR
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 bg-black bg-opacity-90" />
+                <Dialog.Content className="fixed left-[50%] top-[50%] flex h-fit max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] flex-col gap-2 rounded-[6px] border-2 border-border bg-background p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]">
+                  <Dialog.Title className="text-center">Your QR</Dialog.Title>
+                  <Image
+                    src={"/poster1.webp"}
+                    alt="QR Image"
+                    height={450}
+                    width={450}
+                    objectFit="cover"
+                  />
+                  <Dialog.Close className="absolute right-2 top-2">
+                    <X className="text-white opacity-30" />
+                  </Dialog.Close>
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
+
+            <Dialog.Root>
+              <Dialog.Trigger className="flex items-center gap-1 rounded-sm border border-white border-opacity-10 bg-white bg-opacity-5 px-2 text-xs">
+                Edit<Pencil className="w-3"></Pencil>
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 bg-black bg-opacity-90" />
+                <Dialog.Content className="fixed left-[50%] top-[50%] flex h-fit max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] flex-col gap-2 rounded-[6px] border-2 border-border bg-background p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]">
+                  <Dialog.Title className=" m-0 text-[17px] font-medium text-white">
+                    Edit Profile
+                  </Dialog.Title>
+                  <Dialog.Description className="mb-5 mt-[10px] text-sm leading-normal text-white opacity-50">
+                    Make changes to your profile here. Click save when
+                    you&apos;re done.
+                  </Dialog.Description>
+                  {/* Copy paste the below snippet as many times as needed */}
+                  <fieldset className="mb-[15px] flex items-center gap-5">
+                    <label
+                      className="w-[90px] text-right text-[15px] text-primary"
+                      htmlFor="name"
+                    >
+                      Name
+                    </label>
+                    <input
+                      className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] bg-black bg-opacity-10 px-[10px] text-xs leading-none text-primary shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                      id="name"
+                      defaultValue=""
+                    />
+                  </fieldset>
+                  <fieldset className="mb-[15px] flex items-center gap-5">
+                    <label
+                      className="w-[90px] text-right text-[15px] text-primary"
+                      htmlFor="name"
+                    >
+                      Year
+                    </label>
+                    <input
+                      className="shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] bg-black bg-opacity-10 px-[10px] text-xs leading-none text-primary shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                      id="year"
+                      defaultValue=""
+                    />
+                  </fieldset>
+                  <fieldset className="mb-[15px] flex items-center gap-5">
+                    <label
+                      className="w-[90px] text-right text-[15px] text-primary"
+                      htmlFor="name"
+                    >
+                      Branch
+                    </label>
+                    <input
+                      className="shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] bg-black bg-opacity-10 px-[10px] text-xs leading-none text-primary shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                      id="branch"
+                      defaultValue=""
+                    />
+                  </fieldset>
+                  <fieldset className="mb-[15px] flex items-center gap-5">
+                    <label
+                      className="w-[90px] text-right text-[15px] text-primary"
+                      htmlFor="name"
+                    >
+                      Bio
+                    </label>
+                    <textarea
+                      className="shadow-violet7 focus:shadow-violet8 inline-flex h-[70px] w-full flex-1 items-center justify-center rounded-[4px] bg-black bg-opacity-10 px-[10px] text-xs leading-none text-primary shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                      id="bio"
+                      defaultValue=""
+                    />
+                  </fieldset>
+                  <Dialog.Close asChild>
+                    <button className="inline-flex h-[35px] max-w-[200px] items-center justify-center self-end rounded-[4px] bg-green-600 bg-opacity-90 px-[15px] font-medium leading-none text-white hover:bg-green-500 focus:shadow-[0_0_0_2px] focus:shadow-green-700 focus:outline-none">
+                      Save changes
+                    </button>
+                  </Dialog.Close>
+                  <Dialog.Close asChild className="absolute right-2 top-2">
+                    <X className="w-4 text-white opacity-30" />
+                  </Dialog.Close>
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
+          </div>
+
           {/* Card Body */}
           <div className="mt-16 flex w-full flex-col gap-3 overflow-scroll px-4 pb-2">
             {/* Name and Position Div */}
@@ -208,10 +255,9 @@ const Profile = () => {
 
             {/* See more */}
             <div
-              className="SeeMoreOption flex items-center justify-center gap-1 text-sm text-card-foreground"
+              className="SeeMoreOption text-card-foreground flex items-center justify-center gap-1 text-sm"
               onClick={() => {
                 seeMoreTimeline.play();
-                seeLessTimeline.seek(0);
               }}
             >
               <p>See more</p>
@@ -266,10 +312,9 @@ const Profile = () => {
           <ImageCarousel images={images} />
         </div>
         <div
-          className="SeeLessOption mt-2 hidden items-center justify-center gap-1 text-sm text-card-foreground"
+          className="SeeLessOption text-card-foreground mt-2 hidden items-center justify-center gap-1 text-sm"
           onClick={() => {
-            seeLessTimeline.play();
-            seeMoreTimeline.seek(0);
+            seeMoreTimeline.reverse();
           }}
         >
           <p>See less</p>
