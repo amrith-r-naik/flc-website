8cb60221567a7b2704337135b86be531aa1d4a4b
Sathwik <hsathwik002@gmail.com>
2024-08-10 18:48:55 +0530

    feath:Gsap_horizontalScroll effect

---
+++ src/components/landing/ScrollLeftSection.tsx
@@ -0,0 +1,60 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import React, { useRef, useEffect } from "react";
+
+import Footer from "../Footer/Footer";
+import Events from "./Events";
+import TechStack from "./TechStack";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function ScrollLeftSection() {
+  const sectionRef = useRef(null);
+  const triggerRef = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const pin = gsap.fromTo(
+      sectionRef.current,
+      {
+        translateX: "-300vw", 
+      },
+      {
+        translateX: "0",
+        ease: "none",
+        duration: 1,
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: "top top",
+          end: "2000 top",
+          scrub: 0.6,
+          pin: true,
+        },
+      },
+    );
+    return () => {
+      pin.kill();
+    };
+  }, []);
+
+  return (
+    <section className="scroll-section-outer">
+      <div ref={triggerRef}>
+        <div ref={sectionRef} className="scroll-section-inner">
+          <div className="scroll-section">
+            <Events />
+          </div>
+          <div className="scroll-section">
+            <TechStack />
+          </div>
+      
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ScrollLeftSection;
+
+
+
