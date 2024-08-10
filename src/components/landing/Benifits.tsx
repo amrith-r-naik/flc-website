da80e2266a5f87d6203f7383a44459cdb1e3d6ce
Sathwik <hsathwik002@gmail.com>
2024-08-10 18:58:18 +0530

    feath:Benifits_component

--- src/components/landing/Benifits.tsx
+++ src/components/landing/Benifits.tsx
@@ -1,93 +1,106 @@
-"use client"
-import Image from "next/image";
-import gsap from "gsap"
-import { useGSAP } from '@gsap/react';
-import { useRef } from "react";
 
-import sampleImage from "~/assets/images/sample.jpg"
+
+
+
+
+"use client";
+
+import Image, { StaticImageData } from "next/image";
+import sampleImage from "~/assets/images/sample.jpg";
+import Marquee from "../magicui/marquee";
+
 
 const benifits = [
-    {
-        "icon": sampleImage,
-        "title": "Workshops",
-        "content": "The members get free access to all the events and workshops conducted by the Finite Loop club."
-    },
-    {
-        "icon": sampleImage,
-        "title": "Internships",
-        "content": "Get a chance to grab internships and put your skills into use."
-    },
-    {
-        "icon": sampleImage,
-        "title": "Peer to Peer Learning",
-        "content": "Explain your ideas to others and participate in activities through which you can learn from your peers."
-    },
-    {
-        "icon": sampleImage,
-        "title": "Guest Lecture",
-        "content": "Get an opportunity to listen to some of the renowned experts, and engage in discussions."
-    },
-    {
-        "icon": sampleImage,
-        "title": "Real-Time Projects",
-        "content": "Being in this club, you get to work on real time projects, which allows you to bring out your creative side."
-    },
-    {
-        "icon": sampleImage,
-        "title": "Coding Contest",
-        "content": "We ensure to conduct biweekly coding contests, to improve your analytical and problem solving skills."
-    }
-]
+  {
+    icon: sampleImage,
+    title: "Workshops",
+    content:
+      "The members get free access to all the events and workshops conducted by the Finite Loop club.",
+  },
+  {
+    icon: sampleImage,
+    title: "Internships",
+    content: "Get a chance to grab internships and put your skills into use.",
+  },
+  {
+    icon: sampleImage,
+    title: "Peer to Peer Learning",
+    content:
+      "Explain your ideas to others and participate in activities through which you can learn from your peers.",
+  },
+  {
+    icon: sampleImage,
+    title: "Guest Lecture",
+    content:
+      "Get an opportunity to listen to some of the renowned experts, and engage in discussions.",
+  },
+  {
+    icon: sampleImage,
+    title: "Real-Time Projects",
+    content:
+      "Being in this club, you get to work on real time projects, which allows you to bring out your creative side.",
+  },
+  {
+    icon: sampleImage,
+    title: "Coding Contest",
+    content:
+      "We ensure to conduct biweekly coding contests, to improve your analytical and problem solving skills.",
+  },
+];
 
+const firstRow = benifits.slice(0, benifits.length / 2);
+const secondRow = benifits.slice(benifits.length / 2);
 
-function Benifits() {
+const ReviewCard = ({
+  icon,
+  title,
+  content,
+}: {
+  icon: StaticImageData;
+  title: string;
+  content: string;
+}) => {
+  return (
+    <figure className="relative w-80 cursor-pointer overflow-hidden rounded-xl border border-black bg-white p-6 hover:bg-slate-200 dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]">
+      <div className="flex flex-row items-center gap-3">
+        <Image
+          className="rounded-full"
+          src={icon}
+          alt={title}
+          width={48}
+          height={48}
+          layout="intrinsic"
+        />
+        <div className="flex flex-col">
+          <figcaption className="caption font-medium dark:text-white">
+            {title}
+          </figcaption>
+        </div>
+      </div>
+      <blockquote className="caption mt-3">{content}</blockquote>
+    </figure>
+  );
+};
 
-    const ref = useRef(null)
+export function Benifits() {
+  return (
+    <section className="content-container min-h-[80vh] w-full space-y-8  bg-gradient-to-b from-black via-indigo-950 to-black ">
+      <h3 className="text-center text-4xl font-semibold border border-yellow-700 rounded-r-full p-4">Why To Join Us</h3>
 
-    useGSAP(() => {
-        gsap.from(ref.current, {
-            opacity: 0,
-            scale: .5,
-            duration: 1,
-            ease: "power1.inOut",
-            scrollTrigger: {
-                trigger: ref.current,
-                toggleActions: "restart none none reverse"
-            }
-        });
-
-        gsap.from('.benifit', {
-            opacity: 0,
-            y: 100,
-            stagger: {
-                grid: "auto",
-                amount: 1
-            },
-            scrollTrigger: {
-                trigger: ref.current,
-                toggleActions: "restart none none reverse"
-            }
-        });
-
-    }, { scope: ref });
-
-    return (
-        <section className="w-full content-container space-y-8 min-h-[80vh]" ref={ref}>
-            <h3 className='text-4xl font-semibold text-center'>Why To join us</h3>
-
-            <div className="grid grid-cols-3 grid-flow-row gap-4">
-                {
-                    benifits.map((benifit) => (
-                        <div className="rounded-lg bg-gradient border border-yellow-700 p-4 space-y-4 benifit" key={benifit.title}>
-                            <Image width={50} height={50} src={benifit.icon} alt="icon" />
-                            <strong className="text-lg font-semibold">{benifit.title}</strong>
-                            <p className="text-sm">{benifit.content}</p>
-                        </div>
-                    ))
-                }
-            </div>
-        </section>
-    )
+      <div className="w-full flex-1 overflow-hidden">
+        <Marquee pauseOnHover className="[--duration:25s]">
+          {firstRow.map((benefit, index) => (
+            <ReviewCard key={index} {...benefit} />
+          ))}
+        </Marquee>
+        <Marquee reverse pauseOnHover className="[--duration:25s]">
+          {secondRow.map((benefit, index) => (
+            <ReviewCard key={index} {...benefit} />
+          ))}
+        </Marquee>
+      </div>
+    </section>
+  );
 }
 
-export default Benifits
+export default Benifits;
