891d8a205e345e722082ff061dca6df186355768
Sathwik <hsathwik002@gmail.com>
2024-08-10 18:50:33 +0530

    feath:New_Hero_design

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,20 +1,44 @@
+import { ArrowRight,Badge } from "lucide-react";
 import Link from "next/link";
-import { ArrowRight } from "lucide-react";
+
+import { Boxes } from "~/components/ui/background-boxes";
 
 import Button from "~/components/Button";
 
 function Hero() {
-    return (
-        <section className=" w-full min-h-[70vh] grid place-content-center">
-            <div className="content-container text-center  space-y-6">
-                <h1 className="text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#F0C78E] to-[#E98F81]">FINITE LOOP CLUB</h1>
-                <p className="max-w-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus numquam </p>
-                <Button>
-                    <Link href="/regsiter" className="flex gap-2 items-center"><span>Register</span><ArrowRight size={16} /></Link>
-                </Button>
-            </div>
-        </section>
-    )
+  return (
+    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-slate-900">
+      <div className=" pointer-events-none absolute inset-0 z-20 h-full w-full space-y-4 bg-slate-900 [mask-image:radial-gradient(transparent,white)]" />
+
+      <Boxes />
+      <h1 className="title relative z-20  text-white text-center ">NMAMIT<br/>FINITE LOOP CLUB</h1>
+      <p className="relative z-20 mt-4 text-center text-neutral-300 leading-relaxed">
+        We are a Coding club of NMAMIT aiming at Realizing the idea and
+        Inspiring the next!
+        <br />
+      </p>
+      <div className="z-20 mt-4 gap-2 flex flex-row">
+        <Button>
+          <Link
+            href="/register"
+            className="flex items-center gap-2 text-white "
+          >
+            <span>Register</span>
+            <ArrowRight size={16} />
+          </Link>
+        </Button>
+        <Button>
+          <Link
+            href="/register"
+            className="flex items-center gap-2 text-white "
+          >
+            <span> Events</span>
+            <Badge size={16} />
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
