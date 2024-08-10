0cb74f6c3966c6c67245e96b851a2ce7c2ca5e9b
Sathwik <hsathwik002@gmail.com>
2024-08-10 19:10:03 +0530

    fix:landingPage_layout

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,28 +1,21 @@
-import TechStack from "~/components/landing/TechStack";
-import Hero from "~/components/landing/Hero";
-import Benifits from "~/components/landing/Benifits";
-import Projects from "~/components/landing/Projects";
-import Events from "~/components/landing/Events";
-import Hackfest from "~/components/landing/Hackfest";
-import AboutFLC from "~/components/landing/AboutFLC";
 import Footer from "~/components/footer/Footer";
+import Benifits from "~/components/landing/Benifits";
+import { FlagShip } from "~/components/landing/FlagShip";
+import Hero from "~/components/landing/Hero";
+import Projects from "~/components/landing/Projects";
+import ScrollLeftSection from "~/components/landing/ScrollLeftSection";
+import ScrollRightSection from "~/components/landing/ScrollRightSection";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-stretch gap-20">
-      <video
-        src="line-waves.webm"
-        autoPlay
-        muted
-        loop
-        className="absolute top-[30%] -z-10 w-full brightness-75"
-      ></video>
+    <main className="flex flex-col items-stretch gap-10">
       <Hero />
-      <AboutFLC />
-      <Hackfest />
-      <Events />
-      <TechStack />
-      <Projects />
+      <ScrollRightSection />
+      <FlagShip />
+      <div className="bg-gradient-to-b from-black via-indigo-950 to-blue-950">
+        <Projects />
+      </div>
+      <ScrollLeftSection />
       <Benifits />
       <Footer />
     </main>
