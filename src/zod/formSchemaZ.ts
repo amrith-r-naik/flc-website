0c898c782e431cb3a972208c28162b9f00d33035
Rakshith <rakshithx09@gmail.com>
2024-08-11 22:33:37 +0530

    feat: reset password and verify email backend integration

--- src/zod/formSchemaZ.ts
+++ src/zod/formSchemaZ.ts
@@ -63,23 +63,6 @@ const RegisterFormZ =z.object({
     contribution: z.string().min(10, { message: "Please provide more details (at least 10 characters)" }),
   });
 
-  const ResetPasswordFormZ = z.object({
-    email: z
-    .string()
-    .email()
-    .refine((email) => email.endsWith("nmamit.in"), {
-      message: "Email must be a NMAMIT email",
-    }),
-    password: z.string().refine((password) => password.length >= 8, {
-      message: "Password must be atleast 8 characters",
-    }),
-    confirmPassword: z.string(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  });
+export { signUpFormZ, verifyEmailFormZ, loginFormZ,RegisterFormZ,resetPasswordFormZ };
 
-export { signUpFormZ, verifyEmailFormZ, loginFormZ,RegisterFormZ,ResetPasswordFormZ };
 
-export { signUpFormZ, verifyEmailFormZ, loginFormZ,resetPasswordFormZ };
