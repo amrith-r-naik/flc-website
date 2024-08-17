58538efcecd00e5a1d19b693f97aecbc882c9421
Prabhuomkar9 <prabhuomkar9@gmail.com>
2024-08-18 00:18:10 +0530

    [fix] auth pages, events card, 404

--- src/pages/auth/send-verify-email.tsx
+++ src/pages/auth/send-verify-email.tsx
@@ -5,23 +5,16 @@ import SendVerifyEmailForm from "~/components/forms/send-verify";
 
 const SendVerifyEmail: NextPage = () => {
   return (
-    <>
-      {/* <div className="z-0">
-        <Background />
-      </div> */}
-      <>
-        <div className="radialgradient flex  h-screen w-full items-center justify-center">
-          <div className="mx-8 w-4/5  justify-center rounded-lg  bg-white/15 sm:w-96">
-            <div className="m-4">
-              <h1 className=" mb-8 flex justify-center text-center text-4xl">
-                Verify Email
-              </h1>
-              <SendVerifyEmailForm />
-            </div>
-          </div>
+    <div className="radialgradient flex  h-screen w-full items-center justify-center">
+      <div className="mx-8 w-4/5  justify-center rounded-lg  bg-white/15 sm:w-96">
+        <div className="m-4">
+          <h1 className=" mb-8 flex justify-center text-center text-4xl">
+            Verify Email
+          </h1>
+          <SendVerifyEmailForm />
         </div>
-      </>
-    </>
+      </div>
+    </div>
   );
 };
 
--- src/pages/auth/verify-email.tsx
+++ src/pages/auth/verify-email.tsx
@@ -1,50 +1,26 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-import { toast } from "sonner";
+"use client";
 
-import { api } from "~/utils/api";
+import { useRouter } from "next/router";
+import React from "react";
+
+import VerifyEmailForm from "~/components/forms/verify-email";
 
 const VerifyEmail = () => {
   const router = useRouter();
+
   const { token } = router.query;
-  const [verificationToken, setVerificationToken] = useState("");
-  const verifyEmail = api.auth.verifyEmail.useMutation({
-    onSuccess: async () => {
-      toast.success("Email verified successfully!");
-      void router.push("/register");
-    },
-    onError: ({ message }) => {
-      toast.dismiss();
-      toast.error(message);
-    },
-  });
+  const tokenString = Array.isArray(token) ? token[0] : token;
 
-  useEffect(() => {
-    if (token) {
-      // Handle case where token is an array of strings
-      const tokenString = Array.isArray(token) ? token[0] : token;
-      console.log("token string: ", tokenString);
-      setVerificationToken(tokenString!);
-    }
-  }, [token]);
-
-  useEffect(() => {
-    if (verificationToken) {
-      console.log("verificationToken: ", verificationToken);
-      verifyEmail.mutate({ token: verificationToken });
-    }
-  }, [verificationToken, verifyEmail]);
   return (
-    <>
-      <div className="flex h-screen items-center">
-        <div className=" order-1 mx-auto w-4/5 -translate-y-10  justify-center rounded-lg bg-white/15  lg:order-2 ">
-          <Link href="/register" className="">
-            <h2>Back to login!</h2>
-          </Link>
-        </div>
+    <div className="flex h-screen items-center">
+      <div className="mx-auto -translate-y-10  justify-center rounded-lg bg-white/15  lg:order-2">
+        {tokenString ? (
+          <VerifyEmailForm token={tokenString} />
+        ) : (
+          <div className="p-3">Invalid Token</div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
