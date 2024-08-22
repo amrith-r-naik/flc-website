1b29879ac161f472bd34c9f0f070a14b8d1a3bb8
len-mendonca <lenmendonca2003@gmail.com>
2024-08-22 21:46:59 +0530

    refactor: bnefits.tsx

--- src/components/landing/benifits.tsx
+++ src/components/landing/benifits.tsx
@@ -1,87 +1,10 @@
 "use client";
 
-import Image, { type StaticImageData } from "next/image";
-import { useRef } from "react";
-
-import sampleImage from "~/assets/images/sample.jpg";
-
-import Marquee from "../magicui/marquee";
-
-const benifits = [
-  {
-    icon: sampleImage,
-    title: "Workshops",
-    content:
-      "The members get free access to all the events and workshops conducted by the Finite Loop club.",
-  },
-  {
-    icon: sampleImage,
-    title: "Internships",
-    content: "Get a chance to grab internships and put your skills into use.",
-  },
-  {
-    icon: sampleImage,
-    title: "Peer to Peer Learning",
-    content:
-      "Explain your ideas to others and participate in activities through which you can learn from your peers.",
-  },
-  {
-    icon: sampleImage,
-    title: "Guest Lecture",
-    content:
-      "Get an opportunity to listen to some of the renowned experts, and engage in discussions.",
-  },
-  {
-    icon: sampleImage,
-    title: "Real-Time Projects",
-    content:
-      "Being in this club, you get to work on real time projects, which allows you to bring out your creative side.",
-  },
-  {
-    icon: sampleImage,
-    title: "Coding Contest",
-    content:
-      "We ensure to conduct biweekly coding contests, to improve your analytical and problem solving skills.",
-  },
-];
-
-const firstRow = benifits.slice(0, benifits.length / 2);
-const secondRow = benifits.slice(benifits.length / 2);
-
-const ReviewCard = ({
-  icon,
-  title,
-  content,
-}: {
-  icon: StaticImageData;
-  title: string;
-  content: string;
-}) => {
-  return (
-    <figure className="relative w-80 cursor-pointer overflow-hidden rounded-xl border border-black bg-white p-6 hover:bg-slate-200 dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]">
-      <div className="flex flex-row items-center gap-3">
-        <Image
-          className="rounded-full"
-          src={icon}
-          alt={title}
-          width={48}
-          height={48}
-          layout="intrinsic"
-        />
-        <div className="flex flex-col">
-          <figcaption className="caption font-medium dark:text-white">
-            {title}
-          </figcaption>
-        </div>
-      </div>
-      <blockquote className="caption mt-3">{content}</blockquote>
-    </figure>
-  );
-};
+import Image from "next/image";
 
 export function Benifits() {
   return (
-    <section className="relative z-50 mb-44 mt-20 flex h-[140vh] flex-col items-center justify-center px-4 font-sans sm:px-8  md:px-16 xl:px-36">
+    <section className="relative z-50 mb-44 mt-20 flex h-[145vh] flex-col items-center justify-center px-4 pb-4 font-sans  sm:px-8 md:px-16 xl:px-36">
       <div className="line-break "></div>
       <div className="event-bg "></div>
       <p
