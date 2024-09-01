1f45ccbe95ff8e9e23c7e30d3567ae31839d93d8
ahmedmsayeem <msayeemahmed76@gmail.com>
2024-09-01 16:15:00 +0530

    [refac] removed test page

--- src/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import AddCore from '~/components/dashboard/core/addOfficeBearer'
-export default function Test() {
-  return (
-    <div className='p-12'>
-        <AddCore></AddCore>
-    </div>
-  )
-}
